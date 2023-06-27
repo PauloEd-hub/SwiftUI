3c9a6846d54706f54634b7ef5b136ab249d3db3e
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2023-06-27 18:27:35 -0300

    Initial Commit

---
+++ SwiftUIntroducao/SwiftUIntroducaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIntroducaoApp.swift
+//  SwiftUIntroducao
+//
+//  Created by Paulo Eduardo da Silva Cavalcante on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIntroducaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
