11b2bb2b2833452ceb723cd56dda72916c9f51b7
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2023-06-29 19:30:39 -0300

    Criando primeira tela com SwiftUI

--- SwiftUIntroducao/ContentView.swift
+++ SwiftUIntroducao/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack (spacing: 20){
+            Text("Title")
+                .font(.largeTitle)
+            
+            Text("SubTitle")
+                .font(.title)
+                .foregroundColor(Color.gray)
+            
+            Text("Short description of what I am demonstrating goes here.")
+                .frame(maxWidth: .infinity)
+                .font(.title)
+                .foregroundColor(Color.white)
+                .padding()
+                .background(Color.blue)
+            
+            
         }
-        .padding()
     }
 }
 
